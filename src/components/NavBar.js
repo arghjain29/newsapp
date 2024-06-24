6d503c4af034d859ee1b63b701fd7d6d2b616435
arghjain29 <arghjain1234@gmail.com>
2024-06-24 16:49:37 +0530

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ export default class navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <Link className="navbar-brand" exact to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- src/components/News.js
+++ src/components/News.js
@@ -88,7 +88,7 @@ export default class News extends Component {
   render() {
     return (
       <>
-        <h1 style={{ margin: "35px" }} className='text-center'><strong>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</strong></h1>
+        <h1 style={{ margin: "35px", marginTop: "70px" }} className='text-center'><strong>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</strong></h1>
         {this.state.loading && <Spinner />}
 
         <InfiniteScroll
